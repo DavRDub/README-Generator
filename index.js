b3d3100bd2ce5888d8bd819e1707f907b7a22943
David Whitesel <whiteseldavid@gmail.com>
2021-05-23 17:40:14 -0700

    adding messages to index

---
+++ index.js
@@ -0,0 +1,156 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const standardQuestions = () => {
+    return inquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message:"What is your current GitHub username?",
+      validate(input){
+          if(input===""){
+              return "You must enter a valid username.";
+          } else {
+              return true;
+          }
+      },  
+      
+    },
+    {
+        type = "input",
+        name = "title",
+        message = "Name the title of your current project.",
+        validate(input){
+            if(input===""){
+                return "You must enter a project title."
+            } else {
+                return true;
+            }
+        },  
+    },
+    {
+        type = "input",
+        name = "emailaddress",
+        message = "Please enter your current email address.",
+        validate(input){
+            if(input==="") {
+                return "You must enter a email address.";
+            } else {
+                return true;
+            }
+        },
+    },
+    {
+        type = "input",
+        name = "Projectdescription",
+        message = "Provide a short description of your project.",
+        validate(input) {
+            if(input===""){
+                return "Please provide a description.";
+            }else {
+                return true;
+            }
+        }
+    },
+    {
+        type = "input",
+        name = "implement",
+        message = "Please provide the steps required for implementing your project."
+    },
+    {
+        type = "input",
+        name = "applications",
+        message = "Provide a guide and a few examples showing off the applications of the project."
+    },
+    {
+        type = "input",
+        name = "features",
+        message = "Name a few features of the project."
+    },
+    {
+        type = "input",
+        name = "Credit",
+        message = "Provide a list of any collaborators of the project, if any."
+    },
+    {
+        type = "input",
+        name = "Test",
+        message = "Please provide instructions for testing the project, if any."
+    },
+    {
+        type = "list",
+        name = "License Selection",
+        message = "Please select a license.",
+        choices = ["MIT","GNU GPLv3","Apache"]
+    },
+])
+
+.then((data) => {
+    const fname = `${data.title.toLowerCase().split("").join("")}.md`
+
+    JSON.stringify(data, null, '/t')
+
+    fs.writeFile(fname.generateREADME(data,licenseBadge(data)), (err) =>
+    err ? console.log(data) : console.log("Successful!")
+    );
+});
+
+};
+
+generateREADME = (answers, badge) =>
+`
+${badge}
+# ${answers.title}
+## Description
+${answers.description}
+## Table of Contents
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [Features](#features)
+ - [Credits](#credits)
+ - [Contributing](#contributing)
+ - [Tests](#tests)
+ - [License](#license)
+ - [Questions](#questions)
+## Installation
+${answers.implement}
+## Usage
+${answers.applications}
+## Features
+${answers.features}
+## Credits
+${answers.Credit}
+## Tests
+${answers.Test}
+## License
+This project is licensed under the ${answers.license} license.
+## Questions
+If you have any questions, please use the contact information below:  
+https://github.com/${answers.username}  
+${answers.email}
+`
+
+function licenseBadge(answers) {
+
+    switch(answers.license) {
+        case 'MIT':
+            let badgeMIT = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+            return badgeMIT;
+            break;
+        case 'GNU GPLv3':
+            let badgeGNU = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+            return badgeGNU;
+            break;
+        case 'Apache':
+            let badgeApache =  "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+            return badgeApache;
+            break;
+    }
+}
+
+function init() {
+    basicQuestions();
+        
+}
+
+init();
